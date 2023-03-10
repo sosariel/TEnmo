690b8b301acf7d2749a7cf4d6c92b7e54d57b576
Raymon Briones <raybriones729@gmail.com>
2023-03-10 15:40:05 -0600

    Made Jdbc Transer and transfer model

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -0,0 +1,39 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import javax.sql.DataSource;
+
+public class JdbcTransferDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    //Transfer method in DAO to create an ID to detail transfers as they appear.
+    public Integer createTransfer (Transfer transfer) {
+        String sql = "INSERT INTO public.transfer(\n" +
+                "\ttransfer_type_id, transfer_status_id, account_from, account_to, amount)\n" +
+                "\tVALUES (?, ?, ?, ?, ?, ?);";
+        Integer newId = jdbcTemplate.queryForObject(sql, Integer.class, transfer.getTransferType(), transfer.getTransferStatusId(),
+                transfer.getAccountFrom(), transfer.getAccountTo(), transfer.getAmount());
+
+        return newId;
+    }
+
+
+    private Transfer mapRowToTransfer(SqlRowSet rowSet) {
+        Transfer transfer = new Transfer();
+        transfer.setTransferId(rowSet.getInt("transfer_id"));
+        transfer.setTransferType(rowSet.getInt("transfer_type"));
+        transfer.setTransferStatusId(rowSet.getInt("transfer_status_id"));
+        transfer.setAccountFrom(rowSet.getInt("account_from"));
+        transfer.setAccountTo(rowSet.getInt("account_to"));
+        transfer.setAmount(rowSet.getBigDecimal("amount"));
+        return transfer;
+    }
+}
--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -1,4 +1,75 @@
 package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
 //NEW CLASS
 public class Transfer {
+
+    private int transferId;
+    private int transferType;
+    private int transferStatusId;
+    private int accountFrom;
+    private int accountTo;
+    private BigDecimal amount;
+
+    public Transfer() {
+
+    }
+
+    public int getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(int transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public int getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public int getAccountFrom() {
+        return accountFrom;
+    }
+
+    public int getTransferType() {
+        return transferType;
+    }
+
+    public void setTransferType(int transferType) {
+        this.transferType = transferType;
+    }
+
+    public void setAccountFrom(int accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public int getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(int accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Transfer(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public Transfer(BigDecimal amount) {
+        this.amount = amount;
+    }
+
 }
